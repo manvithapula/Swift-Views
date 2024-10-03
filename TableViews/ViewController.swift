1c9f50a98f6121d5a313198700f476e3fa36d43b
manvithapula <manvithapula@gmail.com>
2024-10-03 08:33:06 +0530

    Initial Commit

---
+++ TableViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViews
+//
+//  Created by admin64 on 03/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
