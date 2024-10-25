46735b6f8ee1a074a68d11696864f998587d3a81
P MANVITHA RAYAL <113161233+manvithapula@users.noreply.github.com>
2024-10-25 10:55:07 +0530

    Add files via upload

---
+++ TableViews/TableViews/Emoji.swift
@@ -0,0 +1,15 @@
+//
+//  Emoji.swift
+//  TableViews
+//
+//  Created by admin64 on 03/10/24.
+//
+
+import Foundation
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    
+    var usage: String 
+}
